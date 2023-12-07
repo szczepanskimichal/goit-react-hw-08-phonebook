e1cd8a8afc22838ed12fa4583103374a40fbc6d3
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-07 07:14:27 +0100

    Implemented Redux slices for authentication and contacts management.

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,9 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import {
-  fetchContacts,
-  addContact,
-  deleteContact,
-} from '../contacts/contactsSlice';
+import { fetchContacts, addContact, deleteContact } from './actions';
 
 const handlePending = state => {
   state.isLoading = true;
