e46c6028bef1f0e48535c8092a56bf1a6559c148
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-12-07 06:56:24 +0100

    Configure Redux store with persistency for authentication data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './contacts/filterSlice';
+import { authReducer } from './auth/authSlice';
 
-const savedContacts = localStorage.getItem('contacts');
-
-const preloadedState = savedContacts
-  ? { contacts: JSON.parse(savedContacts) }
-  : {};
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
+    auth: persistReducer(persistConfig, authReducer),
   },
-  preloadedState,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
-
-store.subscribe(() => {
-  localStorage.setItem('contacts', JSON.stringify(store.getState().contacts));
-});
+export const persistor = persistStore(store);
